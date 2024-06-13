94566bcdf951cf1edbd426ed542e5d44f89cbf5d
iharita <haritadave53@gmail.com>
2024-06-13 12:18:53 +0530

    init

---
+++ to_do_app/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ to_do_app/lib/screens/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/screens/splash_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
